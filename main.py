4cfa1078f98760c74069ff41206d26c90d9cfc9f
Saurrabhh <79056681+Saurrabhh@users.noreply.github.com>
2021-12-26 13:20:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,33 +10,54 @@ PATH = Service("C:\\Program Files (x86)\\chromedriver.exe")
 driver = webdriver.Chrome(service=PATH)
 driver.get("https://www.instagram.com")
 driver.implicitly_wait(5)
-username = driver.find_element(By.NAME, "username")
 
-# Write your username in place of ****
-username.send_keys("8401530399")
+try:
+    username = driver.find_element(By.NAME, "username")
+    # Write your username in place of ****
+    username.send_keys("8401530399")
 
-password = driver.find_element(By.NAME, "password")
+    password = driver.find_element(By.NAME, "password")
+    # Write your password in place of ****
+    password.send_keys("$Saurabh5499")
+    password.send_keys(Keys.ENTER)
 
-# Write your password in place of ****
-print(password.get_attribute('outerHTML'))
-password.send_keys("$Saurabh5499")
-password.send_keys(Keys.ENTER)
+except:
+    print("Username or Password field not found")
+else:
+    print("Logged In Successfully")
 
-print(driver.current_url)
+# wait 5 seconds
 time.sleep(3)
 
-not_now = driver.find_element(By.CLASS_NAME, "cmbtv")
-not_now.click()
-time.sleep(5)
+try:
+    not_now = driver.find_element(By.CLASS_NAME, "cmbtv")
+    not_now.click()
+    time.sleep(3)
+    notnow = driver.find_element(By.XPATH, "//*[@class='mt3GC']/button[1]")
+    notnow.click()
+except:
+    print("Not Now not found")
 
-notnow = driver.find_element(By.XPATH, "//*[@class='mt3GC']/button[1]")
-notnow.click()
 
-user_id = driver.find_element(By.CLASS_NAME, "gmFkV")
-print(user_id.text)
-time.sleep(1)
+try:
+    user_id = driver.find_element(By.CLASS_NAME, "gmFkV")
+    user_id_text = user_id.text
+    time.sleep(1)
 
-driver.get(f"https://www.instagram.com/{user_id.text}")
+    driver.get(f"https://www.instagram.com/{user_id.text}")
+except:
+    print("User ID not found")
 
-name = driver.find_element(By.CLASS_NAME, "Yk1V7")
-print(name.text)
+
+try:
+    name = driver.find_element(By.CLASS_NAME, "Yk1V7")
+    name_text = name.text
+
+except:
+    print("Name not found")
+
+else:
+    print(f"Hello {name_text}!!\nWelcome to your account: {user_id_text}")
+    driver.back()
+
+
